4d41f75b78b151c4c0a5d6e6116830ac1eb4ff95
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-10-19 23:43:16 +0200

    Add telephonecontext

--- src/contexts/telephone/TelephoneContextProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import { JsxEmit } from "typescript";
-import TelephoneContext from "./TelephoneContext";
-
-interface TelephoneContextProviderProps {
-  children: JSX.Element | JSX.Element[];
-}
